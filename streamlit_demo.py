cea986666436a77136fa378d6b56582121fe369b
Serhat Say <34214176+serhat-say@users.noreply.github.com>
2023-01-05 01:01:36 +0300

    Update streamlit_demo.py

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -15,20 +15,20 @@ time.sleep(3)
 if uploaded_img is not None:
     img = load_img(uploaded_img)
 
-colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+    colorizer_siggraph17 = siggraph17(pretrained=True).eval()
 
-(tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
+    (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
 
 
-# colorizer outputs 256x256 ab map
-# resize and concatenate to original L channel
-img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
+    # colorizer outputs 256x256 ab map
+    # resize and concatenate to original L channel
+    img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
 
-out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+    out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
-# Display images
-st.subheader('Input')
-st.image(img_bw)
+    # Display images
+    st.subheader('Input')
+    st.image(img_bw)
 
-st.subheader('Output')
-st.image(out_img_siggraph17)
+    st.subheader('Output')
+    st.image(out_img_siggraph17)
